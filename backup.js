0893055406fc4d9666e014db065d18ea9bc5e3c6
Aprilian Ihza <ihzaaprilian@gmail.com>
2022-10-24 22:18:23 +0700

    add article content

--- backup.js
+++ backup.js
@@ -1,23 +1,56 @@
+let containerArtikel = document.getElementById("main")
+const inputSearch = document.querySelector("#search")
+const form = document.querySelector("#form")
+const sList = document.querySelector('.search-list')
+
+let getDataArtikel = async (result,index) =>{
+    let response = await fetch("https://6354c808ccce2f8c020d4f4b.mockapi.io/artikel4")
+    let artikel = await response.json()
+
+    artikel.forEach(item => {
+        containerArtikel.innerHTML += `
+        <div class="m-4">
+        <h2 class="my-4">${item.judul}</h2>
+        <img src="./img/article/art-1.png" width="650">
+        <h5 class="mt-4">Kategori : ${item.kategori}</h5><br>
+        <h6>${item.isi}</h6><br>
+        <h6>${item.tanggal}</h6>
+        </div>
+        <br>
+        <br>
+        `
+    })
+}
+getDataArtikel()
+
 // Article Search List Start
 const getSearchArticles = async (URL) => {
     const response = await fetch(URL);
     const articles = await response.json();
     let html = ``;
-
+  
     articles.forEach((item, index) => {
       let htmlList = 
       `<strong>Menampilkan hasil untuk <span style="color: orange;">${inputSearch.value}</span>  : </strong>    
           <hr />
           <div class="span8 d-flex">
             <div class="w-100">
-              <a href=""><b>${item.judul}</b>
+              <a href="./isi-article.html"><b>${item.judul}</b>
             </a>
-              <p><a href=""><span>Tanyadokter.com,</span></a><p class="text">${item.isi_artikel1}</p></p>
+              <h6><a href="./index.html"><span>Tanyadokter.com,</span></a><h6 class="text">${item.isi_artikel1}</h6></h6>
               <hr>
             </div>
           </div>`;
       html += htmlList;
-      listArtikel.innerHTML = html;
+      sList.innerHTML = html;
     });
   };
   // Article Search List End
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let SEARCH_URL = "https://634d5f1bf5d2cc648ea441d9.mockapi.io//articles?judul=";
+    const searchValue = inputSearch.value;
+    getSearchArticles(SEARCH_URL + searchValue);
+  });
+  
